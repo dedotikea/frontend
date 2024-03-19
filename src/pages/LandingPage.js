0a3986fe5b0a36b26162f7303f043c41bacb5671
rayhan gustiar <rayhangustiar@gmail.com>
2024-03-19 10:46:06 +0700

    lagi

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => {
         <div>
             <img src={Logo} alt='Logo.png' width="50%"></img>
             <div>
-                <span>teks penjelasan web-app-euy</span>
+                <span>teks penjelasan web-app</span>
                 <div>
                     <Link to="login">Login</Link>
                     <br/>
